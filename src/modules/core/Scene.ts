f4d7fda80461113f5eb9d9e31a79af75403bdb50
Seain Malkin <dev@seain.me>
2022-10-25 18:08:45 +1000

    Scene class with a list of models for rendering

---
+++ src/modules/core/Scene.ts
@@ -0,0 +1,46 @@
+import { Model } from "../object/Model";
+
+class Scene {
+    // Tuple. Model array indexed by program name
+    models = new Array<[string, Array<Model>]>();
+    // camera
+
+    /**
+     * Adds a model to the scene that will be rendered on the 
+     * next draw iteration.
+     * @param tag Identifies the program used to draw the model
+     * @param model A model to be added to the rendered scene
+     */
+    addModel(tag: string, model: Model) {
+        let index = this.findProgramModelArrayIndex(tag);
+        if (index === -1) {
+            // First time program name encountered.
+            const length = this.models.push([tag, new Array<Model>()]);
+            index = length - 1;
+        }
+        const [ _, models ] = this.models[index];
+        models.push(model);
+    }
+
+    /**
+     * Removes a model associated with a program from the scene.
+     * @param tag Identifies the program used to draw the model
+     * @param model The model to be removed from the scene
+     */
+    removeModel(tag: string, model: Model): void {
+        let index = this.findProgramModelArrayIndex(tag);
+        if (index === -1) return; // No program association
+
+        const [ _, models ] = this.models[index];
+        for (let i = 0; i < models.length; i++) {
+            if (models[i] === model) {
+                models.splice(i, 1);
+                break;
+            }
+        }
+    }
+
+    private findProgramModelArrayIndex(tag: string): number {
+        return this.models.findIndex(([ key, _ ]) => key === tag);
+    }
+}
