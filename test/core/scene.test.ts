8aed43ccd7e730d0865d0f49650b4d711b1f76b4
Seain Malkin <dev@seain.me>
2022-10-25 22:11:05 +1000

    Added remove model test

--- test/core/scene.test.ts
+++ test/core/scene.test.ts
@@ -13,5 +13,16 @@ test('Add Model', () => {
     const model = new Model(new Plane(0), new Material());
     const tag = "test_program";
     scene.addModel(tag, model);
-    expect(scene.models.length).toBe(1);
+    expect(scene.models).toContainEqual([tag, [model]]);
+});
+
+test('Add two models then remove one', () => {
+    const model1 = new Model(new Plane(0), new Material());
+    const model2 = new Model(new Plane(0), new Material());
+    const tag = "test_program";
+    scene.addModel(tag, model1);
+    scene.addModel(tag, model2);
+    expect(scene.models).toContainEqual([tag, [model1, model2]]);
+    scene.removeModel(tag, model1);
+    expect(scene.models).toContainEqual([tag, [model2]]);
 });
